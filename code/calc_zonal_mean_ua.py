b7108a732f4ce3d000e59dbf5b107d4b0bf6ff3f
tarkanbilgeMO <73482471+tarkanbilgeMO@users.noreply.github.com>
2021-06-04 16:48:32 +0100

    Generalized paths in calc_zonal_mean_ua.py

--- code/calc_zonal_mean_ua.py
+++ code/calc_zonal_mean_ua.py
@@ -15,12 +15,16 @@ import glob
 # Global variables
 # ==========================================================================================
 
+# DATADIR = '/badc/cmip6/data/CMIP6/PAMIP/'
+DATADIR = '/badc/cmip6/data/CMIP6/CMIP/'
 var_name = 'ua' #begin with zonal wind, ua
 table_id='Amon'
 experiment = 'piControl'
+# experiment = 'pdSST-pdSIC'
 version_name='latest'
 grid_type = 'gn' #gn is model native grid
-plev_for_analysis = 1000. 
+# Pressure level in Pascal
+plev_for_analysis = 10000. 
            
 # ==========================================================================================
 # Functions
@@ -35,38 +39,26 @@ def calculate_zonal_mean(data, var_name, plev_for_analysis):
     var_winter_mean_plev = var_winter_mean.sel(plev=plev_for_analysis, method='nearest')
     return var_winter_mean_plev
 
-def zonal_plot(data, model_name, var_name, plev_for_analysis):
-    try:
-        data.mean(('month', 'lon')).plot.line(label=model_name)
-    except:
-        data.mean(('month', 'longitude')).plot.line(label=model_name)
-
-    plt.xlim(0., 90.)
-    plt.ylabel(f'Zonal and DJFM mean {var_name} at {plev_for_analysis/100.}hPa')
-    plt.xlabel('Latitude (degrees)')
-    plt.title(f'CMIP6 comparison of DJFM mean zonal-mean {var_name}')
-    plt.legend()
-
 # ==========================================================================================
 # Main
 # ==========================================================================================
 
-model_centres = [os.path.basename(file) for file in glob.glob('/badc/cmip6/data/CMIP6/CMIP/*')]
+model_centres = [os.path.basename(file) for file in glob.glob(f'{DATADIR}*')]
 
 for model_centre_code in model_centres:   
-    model_names = os.listdir(f'/badc/cmip6/data/CMIP6/CMIP/{model_centre_code}')
+    model_names = os.listdir(f'{DATADIR}{model_centre_code}')
     # Loop over model names within a model centre
     for model_name in model_names:
 
         print(f'Processing model {model_name}...')
 
-        ensemble_list = [os.path.basename(file) for file in glob.glob(f'/badc/cmip6/data/CMIP6/CMIP/{model_centre_code}/{model_name}/{experiment}/r*')]    
+        ensemble_list = [os.path.basename(file) for file in glob.glob(f'{DATADIR}{model_centre_code}/{model_name}/{experiment}/r*')]    
         # Loop over ensemble members
         for variant_label in ensemble_list:
             outfile = f'{model_centre_code}_{model_name}_{experiment}_{variant_label}_zonalmean.nc'
 
             # Construct corresponding directory on Jasmin
-            directory = f'/badc/cmip6/data/CMIP6/CMIP/{model_centre_code}/{model_name}/{experiment}/{variant_label}/{table_id}/{var_name}/{grid_type}/{version_name}/'
+            directory = f'{DATADIR}{model_centre_code}/{model_name}/{experiment}/{variant_label}/{table_id}/{var_name}/{grid_type}/{version_name}/'
 
             # Create a list of filenames in an ensemble member
             filenames = glob.glob(f'{directory}*')
